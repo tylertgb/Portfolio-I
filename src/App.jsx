900d9dfbff55e87781e0251e573348be4aaa2195
Tyler Gobka Bright <tgbright88@gmail.com>
2024-10-22 12:47:19 +0000

    developing...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 };
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import theme_pattern from "../../assets/theme_pattern.svg";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <img src={profile_img} alt="" />
+      <div className="hero-profile-box">
+        <img className="profile-img" src={profile_img} alt="" />
+        <img className="profile-pattern" src={theme_pattern} alt="" />
+      </div>
       <h1>
         <span>I'am Bright Gobka,</span> Front-end Developer base in Ghana.
       </h1>
       <p>
-        I am a front-end develope|engineer from Tamale, Ghana with 4 years of experience in multiple companies like Alnyaba Tech and Namibra
+        I am a front-end develope | engineer from Tamale, Ghana with 4 years of
+        experience in multiple companies like Alnyaba Tech and Namibra
       </p>
       <div className="hero-action">
         <div className="hero-connect">Connect With Me</div>
